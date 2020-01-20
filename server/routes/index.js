033ca30a54607774c01f61d5d01916429f2f5b66
Stepanov Max <badsanta2010@gmail.com>
2020-01-21 00:35:39 +0300

    1st commit: Working example

---
+++ server/routes/index.js
@@ -0,0 +1,11 @@
+import Users from '../controllers/user';
+
+export default (app) => {
+  app.get(
+    '/api', (req, res) => res.status(200).send({
+      message: 'Welcome to the BookStore API!'
+    })
+  );
+
+  app.post('/api/users', Users.signUp);
+};
