1e229aaab2a41be36f7ac505cacebcf015b1bdb2
Stepanov Max <badsanta2010@gmail.com>
2020-07-15 01:08:16 +0300

    [AT-19] Add switcher component

---
+++ client/src/components/common/switcher.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="switcher">
+    <ul
+      class="switcher__list"
+    >
+      <li
+        class="switcher__list-item"
+        :class="[
+          { 'switcher__list-item_active': item.checked }
+        ]"
+        v-for="(item, index) in this.dataItems"
+        :key="index"
+      >
+        <input
+          class="switcher__input-radio"
+          type="radio"
+          :id="'id-' + switcherId + '-' + index"
+          :name="'name-' + switcherId"
+          :checked="item.checked"
+          @change="clickRadioButton(index)"
+        >
+        <label
+          class="switcher__label"
+          :class="[
+            { 'switcher__label_active': item.checked }
+          ]"
+          :for="'id-' + switcherId + '-' + index"
+        >
+          {{ item.label }}
+        </label>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Switcher',
+  props: {
+    items: {
+      default: [],
+      type: Array,
+      required: true
+    },
+    switcherId: {
+      default: '',
+      type: String
+    }
+  },
+  data() {
+    return {
+      selectedRadioArray: [],
+      dataItems: this.items
+    }
+  },
+  methods: {
+    clickRadioButton(index) {
+      for (let i = 0; i < this.dataItems.length; i++) {
+        if (i === index) {
+          this.dataItems[i].checked = true;
+        } else {
+          this.dataItems[i].checked = false;
+        }
+      }
+    }
+  },
+};
+</script>
