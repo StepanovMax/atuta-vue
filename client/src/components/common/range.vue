2649d68c6a7a99205f3fb7894603298a7de8c1de
Stepanov Max <badsanta2010@gmail.com>
2020-07-16 15:07:53 +0300

    [AT-19] Add rest of common elements

---
+++ client/src/components/common/range.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="range">
+    <input
+      type="number"
+      class="input range__item range__input"
+      placeholder="От"
+    >
+    <input
+      type="number"
+      class="input range__item range__input"
+      placeholder="До"
+    >
+    <p
+      class="range__text"
+    >
+      ₽
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'range'
+};
+</script>
