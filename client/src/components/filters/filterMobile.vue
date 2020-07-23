7cac1681574faf783af2b94aa3de73a255c88c27
Stepanov Max <badsanta2010@gmail.com>
2020-07-23 14:24:16 +0300

    Revert "[AT-31] Add common fields for Garage"
    This reverts commit 7a3cfb080539d9e6ab390d44f9d4aff6f2fda2f0.

--- client/src/components/filters/filterMobile.vue
+++ client/src/components/filters/filterMobile.vue
@@ -21,43 +21,6 @@
       <div class="filter-mobile__content">
         <div class="form">
           <div class="form__row">
-            <h3 class="title title_h6">
-              Город*
-            </h3>
-            <multiselect
-              v-model="townSelection"
-              :options="townsList"
-              :show-labels="false"
-              :allow-empty="false"
-              :close-on-select="true"
-              :multiple="false"
-              label="label"
-              track-by="label"
-              placeholder="Выберите город"
-            />
-          </div>
-
-          <div class="form__row">
-            <h3 class="title title_h6">
-              Район*
-            </h3>
-            <multiselect
-              v-model="districtSelection"
-              :options="districtsList"
-              :show-labels="false"
-              :allow-empty="true"
-              :close-on-select="false"
-              :multiple="true"
-              label="label"
-              track-by="label"
-              placeholder="Выберите район"
-            />
-          </div>
-
-          <div class="form__row">
-            <h3 class="title title_h6">
-              Тип сделки*
-            </h3>
             <multiselect
               v-model="isBuyOrRent"
               :options="buyItems"
@@ -70,11 +33,7 @@
               placeholder="Купить или аренда? *"
             />
           </div>
-
           <div class="form__row">
-            <h3 class="title title_h6">
-              Тип объекта*
-            </h3>
             <multiselect
               v-model="objectType"
               :options="objectTypes"
@@ -105,7 +64,7 @@
           v-if="
             buyRentValue
               && objectTypeValue
-              && objectTypeValue.slug == 'garageOrParking'
+              && objectTypeValue.slug == 'garage'
           "
         />
 
@@ -117,58 +76,15 @@
           v-if="buyRentValue && objectTypeValue && objectTypeValue.slug == 'commercial'"
         />
 
-        <div class="form__row">
-          <h3 class="title title_h6">
-            Продавцы*
-          </h3>
-          <multiselect
-            v-model="sellerSelection"
-            :options="sellersList"
-            :show-labels="false"
-            :allow-empty="false"
-            :close-on-select="false"
-            :multiple="true"
-            label="label"
-            track-by="label"
-            placeholder="Выберите продавца"
-          />
-        </div>
-
-        <div
-          class="form__row"
-          v-if="buyRentValue && buyRentValue.slug == 'buy'"
-        >
-          <h3 class="title title_h6">
-            Цена*
-          </h3>
-          <range
-            rangeType="price"
-            :rangeData.sync="priceRangeValue"
-          />
-        </div>
-
-        <div
-          class="form__row"
-          v-if="buyRentValue && buyRentValue.slug == 'rent'"
-        >
-          <h3 class="title title_h6">
-            Аренда в месяц
-          </h3>
-          <range
-            rangeType="price"
-            :rangeData.sync="priceRangeValue"
-          />
-        </div>
-
         <div class="filter-mobile__buttons-block">
           <button
-            class="btn filter-mobile__btn btn_blue btn_mini"
+            class="btn filter-mobile__btn btn_blue btn_middle"
           >
             Сбросить
           </button>
 
           <button
-            class="btn filter-mobile__btn btn_blue btn_mini"
+            class="btn filter-mobile__btn btn_blue btn_middle"
             :class="[
               {
                 'btn_disabled': !this.requiredFormItemsIsFilled
@@ -201,7 +117,7 @@ import filterGarage from './filterGarage.vue';
 import filterArea from './filterArea.vue';
 import filterCommercial from './filterCommercial.vue';
 import multiselect from 'vue-multiselect';
-import range from '../common/range.vue'
+
 import { mapState } from 'vuex';
 
 export default {
@@ -214,7 +130,6 @@ export default {
     filterGarage,
     filterArea,
     filterCommercial,
-    range,
   },
   props: {
     isFilterOpen: {
@@ -225,63 +140,6 @@ export default {
   },
   data() {
     return {
-      seller: null,
-      sellersList: [
-        {
-          slug: 'personal',
-          label: 'Собственник',
-        },
-        {
-          slug: 'agency',
-          label: 'Агентство',
-        },
-        {
-          slug: 'builder',
-          label: 'Застройщик',
-        },
-      ],
-      town: null,
-      townsList: [
-        {
-          slug: 'taganrog',
-          label: 'Таганрог',
-        },
-        {
-          slug: 'rostov-on-don',
-          label: 'Ростов-на-Дону',
-        },
-      ],
-      district: null,
-      districtsList: [
-        {
-          slug: 'yuzhny',
-          label: 'Южный',
-        },
-        {
-          slug: 'center',
-          label: 'Центр',
-        },
-        {
-          slug: 'zapadny',
-          label: 'Западный',
-        },
-        {
-          slug: 'severo-zapadny',
-          label: 'Северо-западный',
-        },
-        {
-          slug: 'severny',
-          label: 'Северный',
-        },
-        {
-          slug: 'vostochny',
-          label: 'Востоный',
-        },
-        {
-          slug: 'suburb',
-          label: 'Пригород',
-        },
-      ],
       buyRentValue: null,
       buyItems: [
         {
@@ -308,7 +166,7 @@ export default {
           label: 'Комната',
         },
         {
-          slug: 'garageOrParking',
+          slug: 'garage',
           label: 'Гараж / Машиноместо',
         },
         {
@@ -322,30 +180,18 @@ export default {
       ],
     }
   },
+  watch: {
+    filterData: {
+      handler(value) {
+        // console.log('store.filterData ::', value);
+      },
+      deep: true
+    },
+  },
   computed: {
     ...mapState([
       'filterData',
     ]),
-    townSelection: {
-      cache: false,
-      get() {
-        return this.town;
-      },
-      set(value) {
-        this.town = value;
-        this.filterData.town = value.slug;
-      }
-    },
-    districtSelection: {
-      cache: false,
-      get() {
-        return this.district;
-      },
-      set(value) {
-        this.district = value;
-        this.filterData.district = value;
-      }
-    },
     isBuyOrRent: {
       cache: false,
       get() {
@@ -369,7 +215,7 @@ export default {
     requiredFormItemsIsFilled() {
       let value;
       if (this.buyRentValue && this.objectTypeValue) {
-        if (this.objectTypeValue.slug === 'garageOrParking') {
+        if (this.objectTypeValue.slug === 'garage') {
           if (this.filterData.garageOrParkingData.isGarageOrParking) {
             value = true;
           } else {
@@ -378,26 +224,6 @@ export default {
         }
       }
       return value;
-    },
-    priceRangeValue: {
-      cache: false,
-      get() {
-        return this.priceRange;
-      },
-      set(value) {
-        this.priceRange = value;
-        this.filterData.priceRange = value;
-      }
-    },
-    sellerSelection: {
-      cache: false,
-      get() {
-        return this.seller;
-      },
-      set(value) {
-        this.seller = value;
-        this.filterData.seller = value;
-      }
     },
   },
 };
