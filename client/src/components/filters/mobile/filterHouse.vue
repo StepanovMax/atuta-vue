1cb23912ecfb5c05ea173ea16a6ba6542dd60b49
Stepanov Max <badsanta2010@gmail.com>
2020-07-31 13:16:43 +0300

    [AT-31] Add house filter for mobile version

---
+++ client/src/components/filters/mobile/filterHouse.vue
@@ -0,0 +1,225 @@
+<template>
+  <div class="form">
+
+    <div
+      class="form__row"
+      v-if="
+        filterDataSelected.deal
+        && filterDataSelected.deal.slug == 'buy'
+      "
+    >
+      <h3 class="title title_h6 form__title">
+        Цена
+      </h3>
+      <rangeInput
+        key="houseRangeInputPrice"
+        rangeType="price"
+        :value.sync="filterSelected.price"
+      />
+    </div>
+
+    <div
+      class="form__row"
+      v-if="
+        filterDataSelected.deal
+        && filterDataSelected.deal.slug == 'rent'
+      "
+    >
+      <h3 class="title title_h6 form__title">
+        Аренда в месяц
+      </h3>
+      <rangeInput
+        key="houseRangeRent"
+        rangeType="price"
+        :value.sync="filterSelected.rent"
+      />
+    </div>
+
+    <div
+      class="form__row"
+      v-if="
+        filterDataSelected.deal
+        && filterDataSelected.deal.slug == 'rent'
+      "
+    >
+      <h3 class="title title_h6 form__title">
+        Срок аренды
+      </h3>
+      <checkboxes
+        key="houseRentType"
+        checkboxId="houseRentType"
+        checkboxType="default"
+        :items="filterDataDefaultClone.rentType"
+        :value.sync="filterSelected.rentType"
+      />
+    </div>
+
+    <div class="form__row">
+      <h3 class="title title_h6 form__title">
+        Количество комнат
+      </h3>
+      <checkboxes
+        key="houseRoomsCount"
+        checkboxId="houseRoomsCount"
+        checkboxType="checkboxButtons"
+        :items="filterDataDefaultClone.houseRoomsCount"
+        :value.sync="filterSelected.roomsCount"
+      />
+    </div>
+
+    <div class="form__row">
+      <h3 class="title title_h6 form__title">
+        Тип объекта
+      </h3>
+      <checkboxes
+        key="houseType"
+        checkboxId="houseType"
+        :items="filterDataDefaultClone.houseTypes"
+        :value.sync="filterSelected.type"
+      />
+    </div>
+
+    <div class="form__row">
+      <h3 class="title title_h6 form__title">
+        Вид объекта
+      </h3>
+      <checkboxes
+        key="houseView"
+        checkboxId="houseView"
+        :items="filterDataDefaultClone.appTypes"
+        :value.sync="filterSelected.view"
+      />
+    </div>
+
+    <div
+      v-if="false"
+      class="form__row"
+    >
+      <h3 class="title title_h6 form__title">
+        Дом за городом?
+      </h3>
+      <checkbox
+        key="intoCity"
+        checkboxId="intoCity"
+        :item="filterDataDefaultClone.intoCityType"
+        :value.sync="filterSelected.isIntoCity"
+      />
+    </div>
+
+    <div
+      class="form__row"
+    >
+      <h3 class="title title_h6 form__title">
+        Расстояние до города
+      </h3>
+      <rangeInput
+        key="houseDistance"
+        rangeType="distance"
+        :value.sync="filterSelected.distance"
+      />
+    </div>
+
+    <div
+      class="form__row"
+    >
+      <h3 class="title title_h6 form__title">
+        Площадь дома
+      </h3>
+      <rangeInput
+        key="houseAreaHouse"
+        rangeType="area"
+        :value.sync="filterSelected.areaHouse"
+      />
+    </div>
+
+    <div
+      class="form__row"
+    >
+      <h3 class="title title_h6 form__title">
+        Площадь участка
+      </h3>
+      <rangeInput
+        key="houseAreaLand"
+        rangeType="area"
+        :value.sync="filterSelected.areaLand"
+      />
+    </div>
+
+    <div
+      class="form__row"
+    >
+      <h3 class="title title_h6 form__title">
+        Этажей в доме
+      </h3>
+      <rangeInput
+        key="houseFloorAll"
+        rangeType="floorAll"
+        :value.sync="filterSelected.floorAll"
+      />
+    </div>
+
+    <div
+      class="form__row"
+    >
+      <h3 class="title title_h6 form__title">
+        Материал стен
+      </h3>
+      <checkboxes
+        key="houseWall"
+        checkboxId="houseWall"
+        :items="filterDataDefaultClone.houseWall"
+        :value.sync="filterSelected.wall"
+      />
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import multiselect from 'vue-multiselect'
+import rangeInput from '../../common/rangeInput.vue';
+import checkboxes from '../../common/checkboxes.vue';
+
+export default {
+  name: 'filterApp',
+  components: {
+    rangeInput,
+    checkboxes,
+    multiselect,
+  },
+  data() {
+    return {
+      filterSelected: {},
+    }
+  },
+  watch: {
+    filterSelected: {
+      handler() {
+        this.updateFilterHouseState(this.filterSelected);
+      },
+      deep: true
+    },
+  },
+  computed: {
+    ...mapState([
+      'filterDataDefault',
+      'filterDataSelected',
+    ]),
+    filterDataDefaultClone() {
+      return JSON.parse(JSON.stringify(this.filterDataDefault));
+    },
+  },
+  methods: {
+    updateFilterHouseState(data) {
+      this.$store.commit('updateFilterHouseState', data);
+    },
+    resetFilter() {
+      this.$store.commit('resetFilter');
+    },
+  },
+  created() {
+    this.filterSelected = JSON.parse(JSON.stringify(this.filterDataSelected.house));
+  },
+};
+</script>
