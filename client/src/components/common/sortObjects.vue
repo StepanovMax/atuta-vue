89498d757e347fc7854d9efd3780be18fe231930
Stepanov Max <badsanta2010@gmail.com>
2020-08-01 23:18:56 +0300

    [AT-36] Create new component SortObjects, add styles for itr

---
+++ client/src/components/common/sortObjects.vue
@@ -0,0 +1,147 @@
+<template>
+  <ul class="sort-objects">
+    <li class="sort-objects__item">
+      <button
+        class="
+          btn
+          sort-objects__btn
+          sort-objects__btn_price
+        "
+        :class="{'sort-objects__btn_active': activeButton === 'price'}"
+        @click="sortPrice()"
+      >
+        <span
+          class="sort-objects__btn-text"
+          :class="{'sort-objects__btn-text_active': activeButton === 'price'}"
+        >
+          Цена
+        </span>
+        <iconSortArrows
+          v-if="activeButton === 'price'"
+          :class="'sort-objects__btn-icon'"
+          :arrowWay="sortWayPrice"
+        />
+      </button>
+    </li>
+    <li class="sort-objects__item">
+      <button
+        class="
+          btn
+          sort-objects__btn
+          sort-objects__btn_area
+        "
+        :class="{'sort-objects__btn_active': activeButton === 'area'}"
+        @click="sortArea()"
+      >
+        <span
+          class="sort-objects__btn-text"
+          :class="{'sort-objects__btn-text_active': activeButton === 'area'}"
+        >
+          Площадь
+        </span>
+        <iconSortArrows
+          v-if="activeButton === 'area'"
+          :class="'sort-objects__btn-icon'"
+          :arrowWay="sortWayArea"
+        />
+      </button>
+    </li>
+  </ul>
+</template>
+
+<script>
+import iconSortArrows from '../icons/iconSortArrows.vue'
+
+export default {
+  name: 'sortObjects',
+  model: {
+    prop: 'value',
+    event: 'change'
+  },
+  props: {
+    propObjectsForSorting: {
+      type: Array,
+      default: [],
+      required: true,
+    }
+  },
+  components: {
+    iconSortArrows,
+  },
+  data() {
+    return {
+      dataObjectsForSorting: this.propObjectsForSorting,
+      sortWayArea: 'less',
+      sortWayPrice: 'less',
+      activeButton: 'price',
+    }
+  },
+  methods: {
+    sortArea() {
+      if (this.sortWayArea === 'less') {
+        this.dataObjectsForSorting.sort(
+          (a, b) => {
+            if (parseInt(a.area) < parseInt(b.area)) {
+              return -1;
+            }
+            if (parseInt(a.area) > parseInt(b.area)) {
+              return 1;
+            }
+            return 0
+          }
+        );
+        this.sortWayArea = 'more';
+      } else if (this.sortWayArea === 'more') {
+        this.dataObjectsForSorting.sort(
+          (a, b) => {
+            if (parseInt(a.area) < parseInt(b.area)) {
+              return 1;
+            }
+            if (parseInt(a.area) > parseInt(b.area)) {
+              return -1;
+            }
+            return 0
+          }
+        );
+        this.sortWayArea = 'less';
+      }
+      this.activeButton = 'area';
+      this.$emit('update:value', this.dataObjectsForSorting);
+    },
+    sortPrice() {
+      if (this.sortWayPrice === 'less') {
+        this.dataObjectsForSorting.sort(
+          (a, b) => {
+            if (parseInt(a.price) < parseInt(b.price)) {
+              return -1;
+            }
+            if (parseInt(a.price) > parseInt(b.price)) {
+              return 1;
+            }
+            return 0
+          }
+        );
+        this.sortWayPrice = 'more';
+      } else if (this.sortWayPrice === 'more') {
+        this.dataObjectsForSorting.sort(
+          (a, b) => {
+            if (parseInt(a.price) < parseInt(b.price)) {
+              return 1;
+            }
+            if (parseInt(a.price) > parseInt(b.price)) {
+              return -1;
+            }
+            return 0
+          }
+        );
+        this.sortWayPrice = 'less';
+      }
+      this.activeButton = 'price';
+      this.$emit('update:value', this.dataObjectsForSorting);
+    },
+  },
+  mounted() {
+    this.sortPrice();
+  },
+};
+</script>
