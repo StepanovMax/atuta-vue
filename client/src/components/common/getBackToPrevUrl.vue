ac7ffaff12de374914296d3d851feffecc3288c8
Stepanov Max <badsanta2010@gmail.com>
2020-08-04 09:49:26 +0300

    [AT-35] Add getBack button

---
+++ client/src/components/common/getBackToPrevUrl.vue
@@ -0,0 +1,46 @@
+<template>
+  <a
+    href="javascript: history.go(-1)"
+    class="get-back-to-prev-url"
+  >
+    <iconArrowLeft
+      class="get-back-to-prev-url__icon"
+    />
+    <span class="get-back-to-prev-url__text">
+      Вернуться
+    </span>
+  </a>
+</template>
+
+<script>
+import iconArrowLeft from '../icons/iconArrowLeft.vue'
+
+export default {
+  name: 'getBackToPrevUrl',
+  components: {
+    iconArrowLeft,
+  },
+  // model: {
+  //   prop: 'value',
+  //   event: 'change'
+  // },
+  props: {
+    // item: {
+    //   default: {},
+    //   type: Object,
+    //   required: true
+    // },
+  },
+  data() {
+    return {
+      // dataIsIconFilled: false
+    }
+  },
+  methods: {
+    // returnToPreviousUrl() {
+    //   console.log('dataIsIconFilled', this.dataIsIconFilled);
+    //   this.dataIsIconFilled = !this.dataIsIconFilled;
+    // }
+  },
+};
+</script>
