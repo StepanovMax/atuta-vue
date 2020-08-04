1de68714f59e52359a13294f7daaff7ba7264a5e
Stepanov Max <badsanta2010@gmail.com>
2020-08-04 16:06:25 +0300

    [AT-22] Fix search field view

--- client/src/components/header/headerBottom.vue
+++ client/src/components/header/headerBottom.vue
@@ -2,28 +2,20 @@
   <div class="header-bottom">
     <div class="header-bottom__container">
       <Logo location="header" />
-      <p
-        class="paragraph"
-        style="
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          font-size: 14px;
-        "
-      >
-        Скоро здесь будет поле поиска.
-      </p>
+      <searchMain />
     </div>
   </div>
 </template>
 
 <script>
 import Logo from '../logo.vue'
+import searchMain from '../common/searchMain.vue'
 
 export default {
   name: 'HeaderBottom',
   components: {
     Logo,
+    searchMain,
   },
 };
 </script>
