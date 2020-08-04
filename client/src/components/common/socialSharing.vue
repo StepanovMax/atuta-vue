721a29f82ea12170ab26c3a454a8708150f3ca38
Stepanov Max <badsanta2010@gmail.com>
2020-08-04 14:46:13 +0300

    [AT-35] Add social icons for sharing

---
+++ client/src/components/common/socialSharing.vue
@@ -0,0 +1,74 @@
+
+<template>
+  <div class="social-sharing">
+    <p class="social-sharing__title">
+      Поделиться:
+    </p>
+    <ul class="social-sharing__list">
+      <li>
+        <a
+          href=""
+          title="Поделиться Вконтакте"
+          @click.stop.prevent
+        >
+          <iconSocialVK />
+        </a>
+      </li>
+      <li>
+        <a
+          href=""
+          title="Поделиться в Facebook"
+          @click.stop.prevent
+        >
+          <iconSocialFB />
+        </a>
+      </li>
+      <li>
+        <a
+          href=""
+          title="Поделиться в Одноклассники"
+          @click.stop.prevent
+        >
+          <iconSocialOK />
+        </a>
+      </li>
+      <li>
+        <a
+          href=""
+          title="Поделиться в Whatsapp"
+          @click.stop.prevent
+        >
+          <iconSocialWhatsapp />
+        </a>
+      </li>
+      <li>
+        <a
+          href=""
+          title="Поделиться в Viber"
+          @click.stop.prevent
+        >
+          <iconSocialViber />
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import iconSocialVK from '../icons/social/iconSocialVK.vue';
+import iconSocialFB from '../icons/social/iconSocialFB.vue';
+import iconSocialOK from '../icons/social/iconSocialOK.vue';
+import iconSocialViber from '../icons/social/iconSocialViber.vue';
+import iconSocialWhatsapp from '../icons/social/iconSocialWhatsapp.vue';
+
+export default {
+  name: 'socialSharing',
+  components: {
+    iconSocialVK,
+    iconSocialFB,
+    iconSocialOK,
+    iconSocialViber,
+    iconSocialWhatsapp,
+  },
+};
+</script>
