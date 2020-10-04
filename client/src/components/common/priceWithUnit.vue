990758bbc45569631ee1d5b90becfa4b882c56fd
Stepanov Max <badsanta2010@gmail.com>
2020-10-04 15:49:52 +0300

    [AT-39] Remove FontAwesome, replace rouble icon

--- client/src/components/common/priceWithUnit.vue
+++ client/src/components/common/priceWithUnit.vue
@@ -3,38 +3,29 @@
     <p class="price-with-unit__number">
       {{ gFormatPrice(propNumber) }}&nbsp;
     </p>
-    <font-awesome-icon
-      class="price-with-unit__text"
-      :icon="myIcon"
-    />
+    <iconRouble />
   </div>
 </template>
 
 <script>
 import unit from './unit.vue';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faRubleSign } from '@fortawesome/free-solid-svg-icons';
+import iconRouble from '../icons/iconRouble.vue';
 
 export default {
   name: 'priceWithUnit',
   components: {
     unit,
-    FontAwesomeIcon,
+    iconRouble,
   },
   data () {
     return {
-      myIcon: faRubleSign,
+      
     }
   },
   props: {
     propNumber: {
       type: Number,
       default: 0,
-      required: true,
-    },
-    propUnit: {
-      type: String,
-      default: '',
       required: true,
     },
   },
