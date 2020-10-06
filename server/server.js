dc406f7aeb569bbdbd34fd95936ee241a6a06aec
Stepanov Max <badsanta2010@gmail.com>
2020-10-06 15:09:37 +0300

    [AT-39] Add config Cors to server

--- server/server.js
+++ server/server.js
@@ -19,6 +19,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 routes(app);
 
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
 server.listen(
   port,
   hostname,
