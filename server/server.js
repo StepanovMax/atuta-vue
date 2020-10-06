34f977f88b8be9f4c07d7019063c2a558f5a169a
Stepanov Max <badsanta2010@gmail.com>
2020-10-06 14:29:11 +0300

    [AT-39] Add config Cors to server

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const port = 9001;
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors);
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
