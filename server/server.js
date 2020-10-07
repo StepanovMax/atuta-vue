a515e63f1c8092e6e4c036e7266486e060c9f700
Stepanov Max <badsanta2010@gmail.com>
2020-10-07 13:56:21 +0300

    [AT-94] Add cors to server

--- server/server.js
+++ server/server.js
@@ -1,30 +1,47 @@
 import http from 'http';
+import { Router } from 'express';
 import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import routes from './routes';
 
+const corsOptions = {
+  origin: 'http://localhost:9001',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 const hostname = '127.0.0.1';
 const port = 9001;
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors());
+app.use(cors({origin: true}));
 app.options('*', cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const router = Router();
+
 routes(app);
 
 
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.all('*', function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   console.log('test');
+//   next();
+// });
+
+router.get(
+  '/',
+  (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(200).send({
+      message: 'Хей-хей ?!'
+    });
+  }
+);
 
 
 server.listen(
