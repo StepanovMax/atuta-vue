d144b179a969257dab62579459d1147326c0125f
Stepanov Max <badsanta2010@gmail.com>
2020-10-13 21:08:19 +0300

    Remove minifying for the bundle

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -84,9 +84,9 @@ module.exports = {
     new VueLoaderPlugin(),
     // new Dotenv(),
   ],
-  optimization: {
-    minimize: true
-  },
+  // optimization: {
+  //   minimize: true
+  // },
   // resolve: {
   //   alias: {
   //     'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
