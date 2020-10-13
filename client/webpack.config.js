ba660ce6b2f33c9aedb1b2c362280675f143bb86
Stepanov Max <badsanta2010@gmail.com>
2020-10-13 20:56:17 +0300

    Add minifying for the bundle

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -84,6 +84,9 @@ module.exports = {
     new VueLoaderPlugin(),
     // new Dotenv(),
   ],
+  optimization: {
+    minimize: true
+  },
   // resolve: {
   //   alias: {
   //     'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
