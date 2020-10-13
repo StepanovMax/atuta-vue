4270f0b018448bf828d0f1e63d8593157b3d499c
Stepanov Max <badsanta2010@gmail.com>
2020-10-13 20:33:47 +0300

    Fix prod host

--- client/src/plugins/getHost.js
+++ client/src/plugins/getHost.js
@@ -7,7 +7,7 @@ const getHost = {
           if (process.env.NODE_ENV === 'development') {
             host = 'http://dev-api.atyta.ru:9001';
           } else if (process.env.NODE_ENV === 'production') {
-            host = 'http://prod-api.atyta.ru:9001';
+            host = 'http://dev-api.atyta.ru:9001';
           } else {
             host = 'http://127.0.0.1:9001';
           }
