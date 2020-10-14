a03616b24c282714623be989d7e1519ace71d696
Stepanov Max <badsanta2010@gmail.com>
2020-10-14 17:39:43 +0300

    [AT-39] Create company page template

---
+++ client/src/components/pages/companyPage.vue
@@ -0,0 +1,62 @@
+<template>
+  <div
+    id="companyPage"
+    class="companies-page"
+  >
+
+    <adsLeft />
+
+    <div class="companies-page__wrap">
+
+      <header class="companies-page__header">
+        <h1 class="companies-page__header-title">
+          {{ companyData.name.label }}
+        </h1>
+      </header>
+
+      {{ companyData }}
+
+    </div>
+
+    <adsRight />
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+import grid from '../grid.vue';
+import adsLeft from '../adsLeft.vue';
+import adsRight from '../adsRight.vue';
+import filterDesktop from '../filters/filterDesktop.vue';
+
+export default {
+  name: 'companyPage',
+  components: {
+    adsLeft,
+    adsRight,
+  },
+  // props: {
+  //   companyData: {
+  //     type: Object,
+  //     default: {},
+  //     required: true,
+  //   },
+  // },
+  data() {
+    return {
+      companyData: null,
+    }
+  },
+  beforeMount(){
+    // this.getSameObjects();
+    // If it's a route transition then load an object through params.
+    if (this.$route.params.companyData) {
+      this.companyData = this.$route.params.companyData;
+    } else {
+      // this.getObjectOnPageReload();
+    }
+  },
+};
+</script>
