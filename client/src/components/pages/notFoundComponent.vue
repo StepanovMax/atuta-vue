95396c856ef23049360d9724269bea2d103ef43d
Stepanov Max <badsanta2010@gmail.com>
2020-10-16 18:02:42 +0300

    [AT-97] Add styles for 404 page

--- client/src/components/pages/notFoundComponent.vue
+++ client/src/components/pages/notFoundComponent.vue
@@ -10,15 +10,40 @@
 
       <header class="not-found-page__header">
 
-        <h1 class="not-found-page__title">
-          404
-        </h1>
+        <div class="not-found-page__header-top">
 
-        <h1 class="not-found-page__subtitle">
-          Страница не найдена
-        </h1>
+          <breadcrumbs />
+
+        </div>
+
+        <div class="not-found-page__header-bottom">
+
+          <h1 class="not-found-page__title">
+            404
+          </h1>
+
+          <h2 class="not-found-page__subtitle">
+            Страница не найдена
+          </h2>
+
+        </div>
 
       </header>
+
+      <div class="not-found-page__content">
+        <p class="paragraph">
+          Ой! Такой страницы на нашем сайте нет :(
+        </p>
+
+        <p class="paragraph">
+          Наверное, вы ошиблись при наборе адреса или перешли по неверной ссылке.
+        </p>
+
+        <p class="paragraph">
+          Не расстраивайтесь, выход есть!<br/>
+          Перейдите на <router-link :to="{name: 'homePage'}" title="Главная">главную страницу</router-link>.
+        </p>
+      </div>
 
     </div>
 
@@ -30,12 +55,14 @@
 <script>
 import adsLeft from '../adsLeft.vue';
 import adsRight from '../adsRight.vue';
+import breadcrumbs from '../common/breadcrumbs.vue';
 
 export default {
   name: 'notFoundComponent',
   components: {
     adsLeft,
     adsRight,
+    breadcrumbs,
   },
 };
 </script>
