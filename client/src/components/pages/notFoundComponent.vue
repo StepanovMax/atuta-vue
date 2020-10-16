54d0871573ae0ffb3eaca7bb37ba3546b1a767e7
Stepanov Max <badsanta2010@gmail.com>
2020-10-16 17:51:30 +0300

    [AT-97] Add 404 page

--- client/src/components/pages/notFoundComponent.vue
+++ client/src/components/pages/notFoundComponent.vue
@@ -1,37 +1,41 @@
 <template>
-  <h1 class="title">
-    404
-  </h1>
+  <div
+    id="notFoundPage"
+    class="not-found-page"
+  >
+
+    <adsLeft />
+
+    <div class="not-found-page__wrap">
+
+      <header class="not-found-page__header">
+
+        <h1 class="not-found-page__title">
+          404
+        </h1>
+
+        <h1 class="not-found-page__subtitle">
+          Страница не найдена
+        </h1>
+
+      </header>
+
+    </div>
+
+    <adsRight />
+
+  </div>
 </template>
 
 <script>
+import adsLeft from '../adsLeft.vue';
+import adsRight from '../adsRight.vue';
 
 export default {
   name: 'notFoundComponent',
   components: {
-    // iconArrowLeft,
-  },
-  model: {
-    // prop: 'value',
-    // event: 'change'
-  },
-  props: {
-    // item: {
-    //   default: {},
-    //   type: Object,
-    //   required: true
-    // },
-  },
-  data() {
-    return {
-      // dataIsIconFilled: false
-    }
-  },
-  methods: {
-    // returnToPreviousUrl() {
-    //   console.log('dataIsIconFilled', this.dataIsIconFilled);
-    //   this.dataIsIconFilled = !this.dataIsIconFilled;
-    // }
+    adsLeft,
+    adsRight,
   },
 };
 </script>
