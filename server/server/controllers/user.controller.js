fc16148e5fee018652b440a224ca055cf0e50088
Stepanov Max <badsanta2010@gmail.com>
2021-02-07 19:45:34 +0300

    [AT-dx0f0c] Remove Login field

--- server/server/controllers/user.controller.js
+++ server/server/controllers/user.controller.js
@@ -90,7 +90,7 @@ const verifyRegistrationLink = async (req, res) => {
 
 
 const registration = async (req, res, file) => {
-  console.log('registration ::');
+  console.log('registration ::', req.body.userData);
   // Collect the user data.
   const userData = req.body.userData;
   // 1. Check for an empty data.
