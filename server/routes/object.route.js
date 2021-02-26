20e00cd47b5265bbb041fd09dd94ae5e17817dab
Stepanov Max <badsanta2010@gmail.com>
2021-02-26 19:56:05 +0300

    [AT-en2r2y] Add ability to add objects to the DB

--- server/routes/object.route.js
+++ server/routes/object.route.js
@@ -1,17 +1,105 @@
 import { Router } from 'express';
 import multer from 'multer';
 import { create } from '../server/controllers/object.controller';
+import fs from 'fs';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import db from '../server/models';
 
-let storageConfig = multer.diskStorage({
+dotenv.config({
+  path: `../env/.env.${process.env.NODE_ENV}`
+});
+
+const Item = db.item;
+
+const jwtSecret = process.env.jwt_secret;
+
+// console.log(' ');
+// console.log('  >> ROUTE PING!');
+// console.log(' ');
+
+
+// console.log('  >> Math.random()', Math.random());
+
+
+// const userId = '199';
+// const currentYear = new Date().getFullYear();
+// const currentMonth = new Date().getMonth() + 1;
+
+// let dir;
+// const objectDirName = Math.random().toString(36).substring(2).substring(0, 4);
+// const dirBase = '../client/uploads/objects/';
+// const dirUser = dirBase + userId + '/' ;
+// const dirYear = dirUser + currentYear  + '/';
+// const dirMonth = dirYear + currentMonth + '/';
+// dir = dirMonth;
+// const dirObject = dirMonth + objectDirName + '/';
+
+
+const folderDetecting = () => {
+  // Если папки юзера не существует,
+  if (!fs.existsSync(dirUser)) {
+    // значит cоздаём папку юзер/год/месяц/объект.
+    return dirMonth;
+  // Если папка юзера существует,
+  } else {
+    // Если папкеи юзер/год не существует,
+    if (!fs.existsSync(dirYear)) {
+      // значит cоздаём папку юзер/год/месяц/объект.
+      return dirMonth;
+    } else {
+      return dirMonth;
+      // // Если папки юзер/год/месяц не существует,
+      // if (!fs.existsSync(dirMonth)) {
+      //   // значит cоздаём папку юзер/год/месяц/объект.
+      //   return dirMonth;
+      // } else {
+      //   // Если папки юзер/год/месяц/объект не существует,
+      //   console.log('  >> Math.random() 1', Math.random());
+      //   const dirObject = dirMonth + Math.random().toString(36).substring(2).substring(0, 4) + '/';
+      //   if (!fs.existsSync(dirObject)) {
+      //     return dirObject;
+      //   } else {
+      //     let objectDirNameNewOne;
+      //     let path;
+      //     do {
+      //       objectDirNameNewOne = Math.random().toString(36).substring(2).substring(0, 4);
+      //       path = dirMonth + objectDirNameNewOne + '/';
+      //     } while (fs.existsSync(path));
+      //     return path;
+      //   }
+      // }
+    }
+  }
+}
+
+let storageConfig;
+let imagesArrayCutted = [];
+let imagePath;
+
+storageConfig = multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, '../client/uploads');
+    const accessToken = req.cookies.accessToken;
+    const decodedAccessToken = jwt.verify(accessToken, jwtSecret);
+    const userId = decodedAccessToken.id;
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth() + 1;
+    const dirRoot = '../client';
+    const dirFolder = '/uploads/objects/';
+    const dirCurrent = dirRoot + dirFolder + userId + '/' + currentYear  + '/' + currentMonth + '/';
+    imagePath = dirFolder + userId + '/' + currentYear + '/' + currentMonth + '/';
+
+
+    fs.mkdirSync(dirCurrent, { recursive: true });
+
+    callback(null, dirCurrent);
+    // callback(null, '../client/uploads');
   },
   filename: function (req, file, callback) {
     console.log(' ');
-    console.log('  storageConfig ::');
-    console.log(' ');
+    console.log('  >> FILE', file);
     const name = 'object-' + Date.now();
-    req.suffix = name;
+    imagesArrayCutted.push(name);
     callback(null, name);
   }
 });
@@ -29,8 +117,34 @@ router.post(
     { name: 'file', maxCount: 10 },
     { name: 'object', maxCount: 1 },
   ]),
-  create,
-);
+  async (req, res) => {
+    const object = JSON.parse(req.body.object);
+    let imagesArray = [];
+    imagesArrayCutted.forEach(
+      item => {
+        imagesArray.push(imagePath + item);
+      }
+    );
+    // console.log(' ');
+    // console.log('  >> imagesArray');
+    // console.log(imagesArray);
+    // console.log(' ');
+    console.log(' ');
+    console.log('  >> object');
+    console.log(typeof object);
+    console.log(' ');
 
+    await Item.create(object).then(
+      data => {
+        res.status(200).send({
+          result: true,
+          data: data,
+        });
+        imagesArray = imagesArrayCutted = [];
+      }
+    );
+
+  }
+);
 
 export default router;
---
+++ server/server/models/item.model.js
@@ -0,0 +1,429 @@
+'use strict';
+module.exports = (sequelize, Sequelize) => {
+
+  const Item = sequelize.define('item', {
+    userName: {
+      type: Sequelize.DECIMAL,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the user number'
+      }
+    },
+    objectType: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the object type'
+      }
+    },
+    deal: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the deal type'
+      }
+    },
+    address: {
+      type: Sequelize.ARRAY(Sequelize.FLOAT),
+      allowNull: {
+        args: false,
+        msg: 'Please enter the address'
+      }
+    },
+    district: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the address'
+      }
+    },
+    onlineShow: {
+      type: Sequelize.BOOLEAN,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the onlineShow value'
+      }
+    },
+    photoGallery: {
+      type: Sequelize.ARRAY(Sequelize.STRING),
+      allowNull: {
+        args: true,
+      }
+    },
+    description: {
+      type: Sequelize.TEXT,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the description value'
+      }
+    },
+    price: {
+      type: Sequelize.BIGINT,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the price value'
+      }
+    },
+    tarif: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the tarif value'
+      }
+    },
+    deposit: {
+      type: Sequelize.BIGINT,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the deposit value'
+      }
+    },
+    rentType: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the rentType value'
+      }
+    },
+    connectionWay: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the connectionWay value'
+      }
+    },
+    phone: {
+      type: Sequelize.BIGINT,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the phone value'
+      }
+    },
+    comfortType: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: true
+      }
+    },
+    comfortBedCount: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: true
+      }
+    },
+    comfortSleepingPlacesCount: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: true
+      }
+    },
+    comfortRoomMultimedia: {
+      type: Sequelize.ARRAY(Sequelize.STRING),
+      allowNull: {
+        args: true
+      }
+    },
+    comfortRoomEquipment: {
+      type: Sequelize.ARRAY(Sequelize.STRING),
+      allowNull: {
+        args: true
+      }
+    },
+    comfortRoom: {
+      type: Sequelize.ARRAY(Sequelize.STRING),
+      allowNull: {
+        args: true
+      }
+    },
+    comfortRoomAdditional: {
+      type: Sequelize.ARRAY(Sequelize.STRING),
+      allowNull: {
+        args: true
+      }
+    },
+    appType: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the app type value',
+      }
+    },
+    appView: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the app view value',
+      }
+    },
+    appRoomsCount: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the app rooms count value',
+      }
+    },
+    appFloor: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the app floor value',
+      }
+    },
+    appFloorAll: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the app floor all value',
+      }
+    },
+    appArea: {
+      type: Sequelize.BIGINT,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the app area value',
+      }
+    },
+    appAreaLiving: {
+      type: Sequelize.BIGINT,
+      allowNull: {
+        args: true,
+      }
+    },
+    appAreaKitchen: {
+      type: Sequelize.BIGINT,
+      allowNull: {
+        args: true,
+      }
+    },
+    appYear: {
+      type: Sequelize.BIGINT,
+      allowNull: {
+        args: true,
+      }
+    },
+    houseType: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the house type value',
+      }
+    },
+    houseRoomsCount: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the house rooms count value',
+      }
+    },
+    houseView: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the house view value',
+      }
+    },
+    houseWall: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: true,
+      }
+    },
+    houseYear: {
+      type: Sequelize.BIGINT,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the house year value',
+      }
+    },
+    houseDistance: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: true,
+      }
+    },
+    houseAreaLand: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the house area land value',
+      }
+    },
+    houseAreaHouse: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the house area value',
+      }
+    },
+    houseFloorAll: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the house floor all value',
+      }
+    },
+    roomArea: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the room area value',
+      }
+    },
+    roomYear: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: true,
+      }
+    },
+    roomFloor: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the room floor value',
+      }
+    },
+    roomFloorAll: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the room floor all value',
+      }
+    },
+    roomRoomsCount: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the rooms count value',
+      }
+    },
+    garageType: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the garage type value',
+      }
+    },
+    garageArea: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the garage area value',
+      }
+    },
+    garageSecurity: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the garage security value',
+      }
+    },
+    garageGarageType: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the garage garage type value',
+      }
+    },
+    garageParkingType: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the garage parking type value',
+      }
+    },
+    sectorType: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the sector type value',
+      }
+    },
+    sectorArea: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the sector area value',
+      }
+    },
+    sectorDistance: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: true,
+      }
+    },
+    sectorFacade: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: true,
+      }
+    },
+    sectorAvailabilityOfBuildings: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: true,
+      }
+    },
+    commercialType: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the commercial type value',
+      }
+    },
+    commercialArea: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the commercial area value',
+      }
+    },
+    commercialYear: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: true,
+      }
+    },
+    commercialClass: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: true,
+      }
+    },
+    commercialDistance: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: true,
+      }
+    },
+    commercialFloor: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the commercial floor value',
+      }
+    },
+    commercialFloorAll: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the commercial floor all value',
+      }
+    },
+    commercialTenant: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the commercial tenant value',
+      }
+    },
+    commercialFacade: {
+      type: Sequelize.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the commercial facade value',
+      }
+    },
+  }, {});
+
+  return Item;
+};
