36beca5523164fc93f2d867cdd6a18052ecc89c9
Stepanov Max <badsanta2010@gmail.com>
2021-04-05 11:36:50 +0300

    [AT-gn5yr8] Add common folder for objects pages

---
+++ client/src/components/pages/object/editObject.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    id="editObject"
+    class="main add-object-page"
+  >
+
+    <adsLeft />
+
+    <errorPage403
+      v-if="!isLoggedIn"
+    />
+
+    <div
+      v-if="isLoggedIn"
+      class="article article_add"
+    >
+      <h1
+        class="
+          title
+          title_h3
+          title_bold
+          form__title_add-object-main
+        "
+      >
+        Редактировать объявление
+      </h1>
+
+    </div>
+
+    <adsRight />
+
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+import adsLeft from '@cmp/adsLeft.vue';
+import adsRight from '@cmp/adsRight.vue';
+import errorPage403 from '@cmp/pages/errors/userNotLoggedInComponent.vue';
+
+export default {
+  name: 'editObject',
+  components: {
+    adsLeft,
+    adsRight,
+    errorPage403,
+  },
+  computed: {
+    ...mapState([
+      'isLoggedIn',
+    ]),
+  },
+};
+</script>
